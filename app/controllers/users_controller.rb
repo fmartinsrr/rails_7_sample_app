53aaa110eab4cfb046216211ba2045f8242c6fa8
Filipe Martins <>
2022-12-30 17:17:39 +0000

    dry user forms and user initialisation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
-	
+	before_action :set_user, only: [:show, :edit, :update]
+
 	def index
   		@users = User.all
 	end
 
 	def show
-		@user = User.find(params[:id])
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
   def new
@@ -17,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def create
-		@user = User.new(params.require(:user).permit(:username))
+		@user = User.new(user_params)
 		if @user.save
 			redirect_to users_url
 		else
@@ -26,12 +25,21 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		@user = User.find(params[:id])
-		if @user.update(params.require(:user).permit(:username))
+		if @user.update(user_params)
 			redirect_to users_url
 		else
 			render 'edit'
 		end
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:username)
 	end
 
 end
