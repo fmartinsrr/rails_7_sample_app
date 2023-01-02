7c6049de2a3bc2273391ea4710c4f310072b1470
Filipe Martins <>
2023-01-02 18:00:58 +0000

    remove default code to set first user to a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,6 @@ class ArticlesController < ApplicationController
 
   def create
 		@article = Article.new(article_params)
-		@article.user = User.first
 		if @article.save
 			redirect_to articles_url
 		else
