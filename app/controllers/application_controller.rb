b16a3d58c264bac685a90b77f41412cbbc13bb6c
Filipe Martins <>
2023-01-09 15:02:39 +0000

    catch exception when getting the user from params include initial docker compose with required services

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,10 @@ class ApplicationController < ActionController::Base
   def current_user
     # a ||= b means assign b to a if a is null or undefined or false
     # For example like: a += b => a = a + b  THEN: a ||= b => a = a || b OR: a = b unless a 
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+    rescue
+    end
   end
 
   def logged_in?
